e822d854af07bbe72939a5663d2ab0624373b53a
sangy00n <dbs1367@postech.ac.kr>
2022-10-06 01:46:01 +0900

    Add: declaration of thread_compare_priority, check_list_preemption in thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -138,4 +138,7 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
+bool thread_compare_priority(const struct list_elem *e1, const struct list_elem *e2, void *aux UNUSED);
+void check_list_preemption(void);
+
 #endif /* threads/thread.h */
