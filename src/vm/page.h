35d637c1113cc1e362e8120d535f9385c38b3766
sch0622 <sch0622@postech.ac.kr>
2022-12-06 06:24:51 +0900

    Update: Makefile.build Fix : in page.h alloc_page_with_zero parameter error Update : pintos, pintos.pm

--- src/vm/page.h
+++ src/vm/page.h
@@ -1,3 +1,6 @@
+#ifndef VM_PAGE_H
+#define VM_PAGE_H
+
 #include "vm/frame.h"
 #include <hash.h>
 #include "filesys/off_t.h"
@@ -32,6 +35,8 @@ void hash_elem_destructor (struct hash_elem *e, void *aux);
 void free_page_table(void);
 void free_page(struct page *page);
 bool alloc_page_with_file(uint8_t *upage, bool writable, struct file *file, off_t offset, size_t read_bytes, size_t zero_bytes);
-bool alloc_page_with_zero(uint8_t upage);
+bool alloc_page_with_zero(uint8_t *upage);
 
 // bool set_page_to_swap (struct page *page, int asdfasdf, bool is_dirty);
+
+#endif
